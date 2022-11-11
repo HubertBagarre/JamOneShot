0418c34e1fa76d04709abe9373d4f843e9c1cc93
Hubert <hubert.ramiandrasoa@gmail.com>
2022-11-11 11:34:25 +0100

    score overlay activation

--- JamGaming/Assets/Scripts/GameManager.cs
+++ JamGaming/Assets/Scripts/GameManager.cs
@@ -12,12 +12,16 @@ public class GameManager : MonoBehaviour
     [SerializeField] private float maxRoundTime = 90f;
     [SerializeField] private float timeBeforeMove = 3f;
     [SerializeField] private float displayDuration = 5f;
-    private WaitForSeconds wait;
+    [SerializeField] private int targetScore = 6;
+    [SerializeField] private GameObject scoreOverlayParent;
+    private WaitForSeconds waitScore;
+    private WaitForSeconds waitMove;
     
     [Header("Current Game")]
     public static List<PlayerInfo> players = new List<PlayerInfo>();
 
     [SerializeField] private Map currentMap;
+    private GameObject currentMapObj;
     [SerializeField] private int currentRound = 0;
     [SerializeField] private float elapsedTime;
     [SerializeField] private bool canMove = true;
@@ -40,7 +44,7 @@ public class GameManager : MonoBehaviour
     {
         Debug.Log($"Setting up  game with {players.Count} players");
         currentRound = -1;
-        wait = new WaitForSeconds(displayDuration);
+        waitScore = new WaitForSeconds(displayDuration);
         foreach (var player in players)
         {
             player.SetupForGame();
@@ -90,60 +94,77 @@ public class GameManager : MonoBehaviour
         //Routine (countdown)
         canMove = false;
     }
+
+    private void CountdownRoutine()
+    {
+        
+    }
     
     private void DisplayScore()
     {
         canMove = true;
-        //Routine (display score for x amount of time)
-        StartNewRound();
+        StartCoroutine(DisplayScoreRoutine());
+        if(!DidPlayerWin()) StartNewRound();
+        
     }
 
     private IEnumerator DisplayScoreRoutine()
     {
-        yield return wait;
+        scoreOverlayParent.SetActive(true);
+        yield return waitScore;
+        scoreOverlayParent.SetActive(false);
         foreach (var player in players)
         {
             player.CanMove(true);
+            player.SetHatActive(true);
         }
         
     }
 
-    private void CheckVictory()
+    private bool DidPlayerWin()
     {
-        
+        return false;
     }
 
     private void CheckToEndRound()
     {
+        Debug.Log($"Checking Round End");
         var alive = 0;
         foreach (var player in players)
         {
             if (player.isAlive) alive++;
         }
+        Debug.Log($"{alive} players Left!");
         if(alive>1) return;
         EndRound();
     }
 
     private void EndRound()
     {
+        Debug.Log($"Ending Round");
         foreach (var player in players)
         {
             player.IncreaseScore();
         }
+        DisplayScore();
     }
 
     private void ChangeMap()
     {
-        Destroy(currentMap);
+        Debug.Log($"Changing Map");
+        if(currentMapObj!=null) Destroy(currentMapObj);
         currentMap = maps[0];
-        Instantiate(currentMap, Vector3.zero, Quaternion.identity);
+        currentMapObj = Instantiate(currentMap, Vector3.zero, Quaternion.identity).gameObject;
     }
 
     public void EliminatePlayer(int playerIndex)
     {
+        Debug.Log($"Killing player {playerIndex}");
         var player = players[playerIndex];
+        player.isAlive = false;
         player.gameObject.SetActive(false);
         player.CanMove(false);
+        player.SetHatActive(false);
         CheckToEndRound();
     }
 }
