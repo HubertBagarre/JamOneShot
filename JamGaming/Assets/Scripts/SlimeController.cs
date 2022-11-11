73f8ad2357c072cc93e48be7fd479663ad806d12
HALLIER Alix <a.hallier@rubika-edu.com>
2022-11-11 02:38:44 +0100

    Jump Only On Wall

--- JamGaming/Assets/Scripts/SlimeController.cs
+++ JamGaming/Assets/Scripts/SlimeController.cs
@@ -11,6 +11,14 @@ public class SlimeController : MonoBehaviour
     [SerializeField] private float launchStrength;
     [SerializeField] private Transform arrow;
 
+    private bool onWall;
+    private Vector2 normalContact;
+
+    private void Start()
+    {
+        onWall = true;
+    }
+
     private void FixedUpdate()
     {
         arrow.localScale = new Vector3(inputAxis.magnitude+1,1,0);
@@ -18,9 +26,20 @@ public class SlimeController : MonoBehaviour
         arrow.rotation = Quaternion.Euler(0,0,Vector2.SignedAngle(Vector2.right,inputAxis));
     }
 
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        slimeRb.gravityScale = 0;
+        normalContact = col.GetContact(0).normal;
+        onWall = true;
+        slimeRb.velocity = Vector2.zero;
+    }
+
     public void Launch(InputAction.CallbackContext ctx)
     {
+        if (!onWall) return;
         slimeRb.AddForce(inputAxis.normalized*launchStrength,ForceMode2D.Impulse);
+        normalContact = Vector2.zero;
+        onWall = false;
     }
 
     public void OnMoveInput(InputAction.CallbackContext ctx)
