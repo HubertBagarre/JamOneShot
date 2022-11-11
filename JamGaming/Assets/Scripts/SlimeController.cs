16a85d09e07f673f65dfd5b036495de2f4170fbe
HALLIER Alix <a.hallier@rubika-edu.com>
2022-11-11 12:28:39 +0100

    Ajout Sprite + Début Rotation Sprite

--- JamGaming/Assets/Scripts/SlimeController.cs
+++ JamGaming/Assets/Scripts/SlimeController.cs
@@ -10,8 +10,10 @@ public class SlimeController : MonoBehaviour
     [SerializeField] private Rigidbody2D slimeRb;
     [SerializeField] private Vector2 inputAxis;
     [SerializeField] private Transform arrow;
+    [SerializeField] private Transform slimeBody;
     [SerializeField] private bool _onWall;
     [SerializeField] private float borneArrow;
+    [SerializeField] private float stretchAmount;
     [SerializeField] private float speed;
     [SerializeField] private float accelFactor;
     [SerializeField] private float launchStrength;
@@ -36,9 +38,9 @@ public class SlimeController : MonoBehaviour
     private void FixedUpdate()
     {
         if (!canLook) return;
-        arrow.localScale = new Vector3(inputAxis.magnitude+1,1,0);
-        arrow.localPosition = new Vector3(inputAxis.x, inputAxis.y, 0);
-        arrow.rotation = Quaternion.Euler(0,0,Vector2.SignedAngle(Vector2.right,inputAxis));
+        arrow.localPosition = inputAxis*stretchAmount;
+        slimeBody.rotation = Quaternion.Euler(0,0,Vector2.SignedAngle(Vector2.up,inputAxis));
+        slimeBody.localScale = new Vector3(1, 1, 1);
     }
 
     private void OnCollisionEnter2D(Collision2D col)
