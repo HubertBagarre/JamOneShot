4f7793567ccf1c3b98e6fce437ad776082662637
Hubert <hubert.ramiandrasoa@gmail.com>
2022-11-12 13:44:49 +0100

    Sound Code Done

--- JamGaming/Assets/Scripts/UIScripts/UINavigation.cs
+++ JamGaming/Assets/Scripts/UIScripts/UINavigation.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,23 +6,34 @@ using UnityEngine.SceneManagement;
 
 public class UINavigation : MonoBehaviour
 {
+    private SoundManager sm;
+
+    private void Start()
+    {
+        sm = SoundManager.instance;
+    }
+
     public void GoToLobby()
     {
+        sm.PlaySound(5);
         SceneManager.LoadScene(2);
     }
 
     public void GoToCredits()
     {
+        sm.PlaySound(5);
         SceneManager.LoadScene(1);
     }
 
     public void GoToMenu()
     {
+        sm.PlaySound(5);
         SceneManager.LoadScene(0);
     }
 
     public void GoToBrazil()
     {
+        sm.PlaySound(5);
         Application.Quit();
     }
 }
