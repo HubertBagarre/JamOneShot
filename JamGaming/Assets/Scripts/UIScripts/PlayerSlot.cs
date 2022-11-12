e7b1efc1529a05eb8d135c34b7ce18a0bf29b30a
Hubert <hubert.ramiandrasoa@gmail.com>
2022-11-12 10:48:20 +0100

    hides mdoel in lobby better lobby

--- JamGaming/Assets/Scripts/UIScripts/PlayerSlot.cs
+++ JamGaming/Assets/Scripts/UIScripts/PlayerSlot.cs
@@ -9,17 +9,21 @@ public class PlayerSlot : MonoBehaviour
 {
     public PlayerInfo playerInfo;
     [SerializeField] private Image modelImage;
+    [SerializeField] private Image hatImage;
     [SerializeField] private TextMeshProUGUI playerNameText;
     [SerializeField] private GameObject readyPanel;
+    [SerializeField] private GameObject faceObject;
 
     private void Start()
     {
         readyPanel.SetActive(false);
+        faceObject.SetActive(false);
     }
 
     public void UpdatePlayerReady()
     {
         playerNameText.text = $"Player {playerInfo.playerIndex}";
+        faceObject.SetActive(true);
     }
 
     public void UpdateColor(Color color)
